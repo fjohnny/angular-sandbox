f6e62d45102474631b9ca0cec254f2e5ebe7f720
Johnny Franjo <johnnyfra@mail.com>
2018-11-04 22:32:34 +1100

    Interfaces, Users component, ngOnInit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { stripGeneratedFileSuffix } from "@angular/compiler/src/aot/util";
+import { User } from "../../models/User";
 
 @Component({
   selector: "app-user",
@@ -7,62 +8,24 @@ import { stripGeneratedFileSuffix } from "@angular/compiler/src/aot/util";
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"]
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   //Properties
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
 
-  foo: any;
-  hasKids: boolean;
-  numberArray: number[];
-  stringArray: string[];
-  mixedArray: any[];
-  myTuple: [string, number, boolean];
-  unusable: void;
-  u: undefined;
-  n: null;
+  user: User;
 
   //Methods
-  constructor() {
-    this.firstName = "Johnny";
-    this.lastName = "Franjo";
-    this.age = 50;
-    this.address = {
-      street: "149 High Street";
-      city: "Thomastown";
-      state: "VIC";
-    }
+  constructor() {}
 
-    this.foo = true;
-    this.hasKids = false;
-    this.numberArray = [1, 2, 3];
-    this.stringArray = ["hello", "world"];
-    this.mixedArray = [true, undefined, null, "hello", 22];
-    this.myTuple = ["hello", 2, true];
-    this.unusable = undefined;
-    this.n = null;
-    this.u = undefined;
-
-    console.log("Helo user ...");
-    this.sayHello();
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
-
-    console.log(this.addNUmbers(2, 4));
-  }
-
-  sayHello() {
-    console.log(`Hello ${this.firstName}`);
-  }
-
-  hasBirthday() {
-    this.age += 1;
-  }
-
-  addNUmbers(num1: number, num2: number): number {
-    return num1 + num2;
+  ngOnInit() {
+    this.user = {
+      firstName: "Johnny",
+      lastName: "Franjo",
+      age: 50,
+      address: {
+        street: "2 Blossom Park Drive",
+        city: "Mill Park",
+        state: "VIC"
+      }
+    };
   }
 }
