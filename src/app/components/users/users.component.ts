4382442d0521d66a8087ec4de1466ad604c2c401
Johnny Franjo <johnnyfra@mail.com>
2018-11-04 22:46:37 +1100

    *ngFor directive

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { User } from "../../models/User";
 
 @Component({
   selector: "app-users",
@@ -6,9 +7,57 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+
   constructor() {}
 
   ngOnInit() {
-    console.log("init ...");
+    this.users = [
+      {
+        firstName: "Johnny",
+        lastName: "Franjo",
+        age: 49,
+        address: {
+          street: "2 Blossom Park Drive",
+          city: "Mill Park",
+          state: "VIC"
+        }
+      },
+      {
+        firstName: "Vilma",
+        lastName: "Franjo",
+        age: 50,
+        address: {
+          street: "1/2 Blossom Park Drive",
+          city: "Mill Park",
+          state: "VIC"
+        }
+      },
+      {
+        firstName: "Game",
+        lastName: "Freaks",
+        age: 12,
+        address: {
+          street: "149 High Street",
+          city: "Thomastown",
+          state: "VIC"
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: "David",
+      lastName: "Jsckson",
+      age: 25,
+      address: {
+        street: "143 High Street",
+        city: "Thomastown",
+        state: "VIC"
+      }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 }
