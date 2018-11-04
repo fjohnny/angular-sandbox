ec3bd84d6f66f29e34a4a864d6c65766208e738c
Johnny Franjo <johnnyfra@mail.com>
2018-11-05 08:01:13 +1100

    *ngIf directive, optional interface properties, setTimeout

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,56 +8,62 @@ import { User } from "../../models/User";
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: "Johnny",
-        lastName: "Franjo",
-        age: 49,
-        address: {
-          street: "2 Blossom Park Drive",
-          city: "Mill Park",
-          state: "VIC"
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: "Johnny",
+          lastName: "Franjo",
+          age: 49,
+          address: {
+            street: "2 Blossom Park Drive",
+            city: "Mill Park",
+            state: "VIC"
+          }
+        },
+        {
+          firstName: "Vilma",
+          lastName: "Franjo",
+          age: 50,
+          address: {
+            street: "1/2 Blossom Park Drive",
+            city: "Mill Park",
+            state: "VIC"
+          }
+        },
+        {
+          firstName: "Game",
+          lastName: "Freaks"
+          // age: 12,
+          // address: {
+          //   street: "149 High Street",
+          //   city: "Thomastown",
+          //   state: "VIC"
+          }
         }
-      },
-      {
-        firstName: "Vilma",
-        lastName: "Franjo",
-        age: 50,
-        address: {
-          street: "1/2 Blossom Park Drive",
-          city: "Mill Park",
-          state: "VIC"
-        }
-      },
-      {
-        firstName: "Game",
-        lastName: "Freaks",
-        age: 12,
-        address: {
-          street: "149 High Street",
-          city: "Thomastown",
-          state: "VIC"
-        }
-      }
-    ];
+      ];
 
-    this.addUser({
-      firstName: "David",
-      lastName: "Jsckson",
-      age: 25,
-      address: {
-        street: "143 High Street",
-        city: "Thomastown",
-        state: "VIC"
-      }
-    });
+      this.loaded = true;
+    }, 3000);
+
+    // this.addUser({
+    //   firstName: "David",
+    //   lastName: "Jackson",
+    //   age: 25,
+    //   address: {
+    //     street: "143 High Street",
+    //     city: "Thomastown",
+    //     state: "VIC"
+    //   }
+    // });
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  // addUser(user: User) {
+  //   this.users.push(user);
   }
 }
